5505767632f49966b31fa356fb7c3304842c0490
aelabd <elabd1996@gmail.com>
2020-08-12 14:23:42 +0200

    Added Dummy data

--- Schema/schema.js
+++ Schema/schema.js
@@ -2,6 +2,24 @@ const graphql = require("graphql");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+let books = [
+  {
+    name: "Name of the Wind",
+    genre: "Fantasy",
+    id: "1"
+  },
+  {
+    name: "The Final Empire",
+    genre: "Fantasy",
+    id: "2"
+  },
+  {
+    name: "The Long Earth",
+    genre: "Sci-Fi",
+    id: "3"
+  }
+];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
