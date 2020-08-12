397d28111b57eb810c9d38fcc0d5102eabb65b6a
aelabd <elabd1996@gmail.com>
2020-08-12 14:05:40 +0200

    Added root query

--- Schema/schema.js
+++ Schema/schema.js
@@ -1,12 +1,24 @@
-const  graphql = require('graphql')
+const graphql = require("graphql");
 
-const {GraphQLObjectType,GraphQLString} = graphql;
+const { GraphQLObjectType, GraphQLString } = graphql;
 
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: () =>({
-        id: {type: GraphQLString},
-        name:{type:GraphQLString},
-        genre:{type:GraphQLString}
-    })
-})
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } }
+    }
+  }
+});
+
+
