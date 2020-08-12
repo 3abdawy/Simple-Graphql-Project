6e1a329a2d8ef52d65b3b26a51f6e4f629cc1bcc
aelabd <elabd1996@gmail.com>
2020-08-12 14:14:51 +0200

    Connecting schema with app

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const schema = require("./Schema/schema");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({graphiql:true}));
+app.use("/graphql", graphqlHTTP({ schema }));
 
 app.listen(4000, () => {
   console.log("now Listening for requests on port 4000");
