53c46dacbd22c842c37095527522a418d9d5b252
aelabd <elabd1996@gmail.com>
2020-08-12 17:14:37 +0200

    try querying with graphiql

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const schema = require("./Schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
   console.log("now Listening for requests on port 4000");
